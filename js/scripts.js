2b226a47becaeddc19892621f1c8e8dbf8183602
meghanmorris <meghan@someoddpilot.com>
2015-10-17 11:04:33 -0500

    adding script file

---
+++ js/scripts.js
@@ -0,0 +1,102 @@
+$('.btn').on('click', function() {
+  $(this).find('.editor').toggleClass('editor--active');
+});
+
+var preview = document.getElementById('preview');
+
+var interval;
+var editor = CodeMirror(document.getElementById('editor'), {
+  value: "<html>foo</html>",
+  mode: "text/html",
+  lineNumbers: true,
+  matchBrackets: true,
+  indentWithTabs: true,
+  tabSize: 4,
+  indentUnit: 4
+});
+console.log(editor);
+editor.on('change', function () {
+  clearTimeout(interval);
+  interval = setTimeout(update, 500);
+});
+
+var update = function () {
+  var value = editor.getValue();
+  if ( validate( value ) ) {
+    // remove previous iframe
+    if ( preview.children.length > 0 ) {
+      preview.removeChild( preview.firstChild );
+    }
+    //
+    var iframe = document.createElement( 'iframe' );
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.border = '0';
+    preview.appendChild( iframe );
+    var content = iframe.contentDocument || iframe.contentWindow.document;
+    // workaround for chrome bug
+    // http://code.google.com/p/chromium/issues/detail?id=35980#c12
+    value = value.replace( '<script>', '<script>if ( window.innerWidth === 0 ) { window.innerWidth = parent.innerWidth; window.innerHeight = parent.innerHeight; }' );
+    content.open();
+    content.write( value );
+    content.close();
+  }
+};
+var errorLines = [];
+var widgets = [];
+var validate = function ( value ) {
+  return editor.operation( function () {
+    while ( errorLines.length > 0 ) {
+      editor.removeLineClass( errorLines.shift(), 'background', 'errorLine' );
+    }
+    for ( var i = 0; i < widgets.length; i ++ ) {
+      editor.removeLineWidget( widgets[ i ] );
+    }
+    widgets.length = 0;
+    // remove html
+    var string = '\n';
+    var lines = value.split( '\n' );
+    var lineCurrent = 0, lineTotal = lines.length;
+    while ( lineCurrent < lineTotal && lines[ lineCurrent ].indexOf( '<script>' ) === -1 ) {
+      string += '\n';
+      lineCurrent ++;
+    }
+    var lineStart = lineCurrent ++;
+    while ( lineCurrent < lineTotal && lines[ lineCurrent ].indexOf( '<\/script>' ) === -1 ) {
+      string += lines[ lineCurrent ] + '\n';
+      lineCurrent ++;
+    }
+    //
+    try {
+      var result = esprima.parse( string, { tolerant: true } ).errors;
+      for ( var i = 0; i < result.length; i ++ ) {
+        var error = result[ i ];
+        var message = document.createElement( 'div' );
+        message.className = 'esprima-error';
+        message.textContent = error.message.replace(/Line [0-9]+: /, '');
+        var lineNumber = error.lineNumber - 1;
+        errorLines.push( lineNumber );
+        editor.addLineClass( lineNumber, 'background', 'errorLine' );
+        var widget = editor.addLineWidget(
+          lineNumber,
+          message
+        );
+        widgets.push( widget );
+      }
+    } catch ( error ) {
+      var message = document.createElement( 'div' );
+      message.className = 'esprima-error';
+      message.textContent = error.message.replace(/Line [0-9]+: /, '');
+      var lineNumber = error.lineNumber - 1;
+      errorLines.push( lineNumber );
+      editor.addLineClass( lineNumber, 'background', 'errorLine' );
+      var widget = editor.addLineWidget(
+        lineNumber,
+        message
+      );
+      widgets.push( widget );
+    }
+    return errorLines.length === 0;
+  });
+};
+update();
